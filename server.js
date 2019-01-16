4662ff49ac7db8bfb1eda80f8d9350322f742fa9
dvash999 <myselfandme@gmail.com>
2019-01-16 07:32:53 +0200

    modifying production version heroku

--- server.js
+++ server.js
@@ -7,12 +7,25 @@ const bodyParser = require('body-parser');
 dotenv.config();
 const app = express();
 const router = express.Router();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.get('/contact/send', (req, res) => res.status(204));
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+ 
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 app.post('/contact/send', (req, res) => {
 
@@ -49,4 +62,4 @@ app.post('/contact/send', (req, res) => {
 
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
