8d3a23cc981cb589391e1122aeb6457cb295f669
dvash999 <myselfandme@gmail.com>
2019-01-26 20:39:26 +0200

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
-
-import React, { Component, StrictMode } from 'react';
+import React, {Component, StrictMode} from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PortfolioPage from './components/portfoliopage/PortfolioPage';
 import MeetMe from './components/meetme/MeetMe';
 import Contact from './components/contactme/Contact';
@@ -9,63 +8,74 @@ import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isButtonOn: false,
-      flickerLights: false,
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            isButtonOn: false,
+            flickerLights: false
+        };
+    }
 
-  toggleSwitch = () => {
+    toggleSwitch = () => {
 
-    this.setState({ flickerLights: true});
+        this.setState({flickerLights: true});
 
-    setTimeout(()=> {
-    this.setState(prevState => ({
-      isButtonOn: true
-    }));
-  },1000)
-  }
+        setTimeout(() => {
+            this.setState({isButtonOn: true});
+        }, 1000)
+    }
 
-  
-  render() {
-    return (
-      <div className="app">
-        <StrictMode>
-          {this.state.isButtonOn ? 
-            (<div>
-              <BrowserRouter>
-            <div>
+    render() {
+        return (
+            <div className="app">
+                <StrictMode>
+                    {this.state.isButtonOn
+                        ? (
+                            <div>
+                                <BrowserRouter>
+                                    <div>
 
-              <div className="fade-in">
-              <div>
-                <NavBar/>
-              </div>
+                                        <div className="fade-in">
+                                            <div>
+                                                <NavBar/>
+                                            </div>
 
-              <Switch>
-                <Route path="/" component={Home} exact/>
-                <Route path="/meetme" component={MeetMe} exact/>Meet Me
-                <Route path="/portfoliopage" component={PortfolioPage}exact/>Portfolio
-                <Route path="/contact" component={Contact} exact/>Contact
-              </Switch>
-              </div>
-              </div>
-            </BrowserRouter>
-          </div>)
-          :
-          <div style={{backgroundColor:"black", height:'100vh', padding:'0', margin:'0'}} className={this.state.flickerLights?"flicker-out-1" : null}>
-            <h1 style={{color:'white', textAlign:'center', paddingTop:'250px'}}>SWITCH ME ON.</h1>
-            <div className="switch" onClick={this.toggleSwitch}>
-              <input type="checkbox" name=""/>
-              <label></label>
+                                            <Switch>
+                                                <Route path="/" component={Home} exact/>
+                                                <Route path="/meetme" component={MeetMe} exact/>Meet Me
+                                                <Route exac path="/portfoliopage" component={PortfolioPage}/>Portfolio
+                                                <Route path="/contact" component={Contact} exact/>Contact
+                                            </Switch>
+                                        </div>
+                                    </div>
+                                </BrowserRouter>
+                            </div>
+                        )
+                        : <div
+                            style={{
+                            backgroundColor: "black",
+                            height: '100vh',
+                            padding: '0',
+                            margin: '0'
+                        }}
+                            className={this.state.flickerLights
+                            ? "flicker-out-1"
+                            : null}>
+                            <h1
+                                style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                paddingTop: '250px'
+                            }}>SWITCH ME ON.</h1>
+                            <div className="switch" onClick={this.toggleSwitch}>
+                                <input type="checkbox" name=""/>
+                                <label></label>
+                            </div>
+                        </div>}
+
+                </StrictMode>
             </div>
-          </div>}
-        
-          
-        </StrictMode>
-      </div>
-    );
-  }
+        );
+    }
 }
 export default App;
