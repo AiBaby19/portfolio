84279e316504037b5541d13ce276f35d67068956
dvash999 <myselfandme@gmail.com>
2019-01-30 09:52:11 +0200

    email fixed

--- config/prod.js
+++
@@ -1,8 +0,0 @@
-//prod.JS  PRODUCTION KEYS HERE
-module.exports = {
-  user: process.env.USER,
-  pass: process.env.PASS,
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  refreshToken: process.env.REFRESH_TOKEN,
-};
