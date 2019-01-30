a1a1a8e542f4121ccffdcb9dfa79c2adcf16ee39
dvash999 <myselfandme@gmail.com>
2019-01-30 11:47:27 +0200

    adding production env

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
